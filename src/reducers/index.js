b5c3ec14e9a551789af111374bfd60d085049212
Varun Singh <varun.singh@homeshop18.com>
2020-12-16 21:37:02 +0530

    working on news frontend

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux';
-import Countries from "./countries";
-import Country from "./country";
 import News from "./news";
 
 export default combineReducers({
-  countries: Countries,
-  country: Country,
   news: News,
 });
